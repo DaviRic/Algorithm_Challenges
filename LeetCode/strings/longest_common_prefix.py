756cc99eea93b8732b11628011b48f56531b70b6
DaviCruvel <davi.r.cruvel@gmail.com>
2025-03-13 16:21:43 -0400

    Atualização da condição de parada do loop

--- LeetCode/strings/longest_common_prefix.py
+++ LeetCode/strings/longest_common_prefix.py
@@ -1,6 +1,6 @@
 def longestCommonPrefix(str_list):
     prefix = ''
-    for string in str_list:
+    for string in str_list[1:]:
         for index in range(str_list):
             if string[index] == str_list[index+1][index]:
                 prefix = string[index]
