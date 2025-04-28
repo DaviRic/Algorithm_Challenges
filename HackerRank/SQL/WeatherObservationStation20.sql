35eee9d71212b8d0e55bbaaae2680c0304b7d4e8
DaviRic <davi.r.cruvel@gmail.com>
2025-04-28 12:30:24 -0400

    [feat] Resolução do desafio 'Weather Observation Station 20' do HackerHank

---
+++ HackerRank/SQL/WeatherObservationStation20.sql
@@ -0,0 +1,28 @@
+WITH Ordered AS (
+    SELECT
+        LAT_N,
+        ROW_NUMBER() OVER (ORDER BY LAT_N) AS rn
+    FROM
+        STATION
+),
+Counted AS (
+    SELECT
+        COUNT(*) AS total_rows
+    FROM
+        STATION
+)
+SELECT ROUND(
+    CASE
+        WHEN total_rows % 2 <> 0 THEN(
+            SELECT LAT_N
+            FROM Ordered
+            WHERE rn = (total_rows+1)/2
+        )
+        WHEN total_rows % 2 = 0 THEN (
+            SELECT LAT_N
+            FROM Ordered
+            WHERE rn IN (total_rows/2, (total_rows+1)/2)
+        )
+    END
+    , 4) AS median_lat
+FROM Counted;
