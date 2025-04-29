2134ed3579f46fe8fdd8af4a82ecbb8e63282290
DaviRic <davi.r.cruvel@gmail.com>
2025-04-29 13:33:02 -0400

    [feat] Resolução do desafi o'Top Earners' do HackerRank

---
+++ HackerRank/SQL/TopEarners.sql
@@ -0,0 +1,9 @@
+SELECT 
+    MAX(salary * months) AS max_total_earnings,
+    COUNT(*) 
+FROM 
+    Employee
+WHERE 
+    salary * months = (
+        SELECT MAX(salary * months) FROM Employee
+    );
